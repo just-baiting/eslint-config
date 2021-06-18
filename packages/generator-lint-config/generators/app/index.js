5c3fcee730b55290ec9c88cc4697be87b5dd135b
Michael Nguyen <aqmnguyen@gmail.com>
2021-06-18 15:02:09 +0100

    yeoman generator for lint-config

---
+++ packages/generator-lint-config/generators/app/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Generator = require('yeoman-generator');
+const _ = require('lodash');
+
+// Inject the Install mixin
+_.extend(Generator.prototype, require('yeoman-generator/lib/actions/install'));
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  initalizing() {
+    this.composeWith('@just-baiting/lint-config:eslint-config');
+  }
+};
